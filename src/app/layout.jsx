853f08debca1d8ec67fdd5a4619e05c3d4f7e84c
haywhyvilla <olatunjiayobami575@gmail.com>
2023-09-06 12:54:34 +0100

    last commit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,37 +1,10 @@
-
+"use client"
 import "./globals.css";
 import { Provider } from "react-redux";
 import store from "../redux/store";
 
 
-export const metadata = {
-    title: "FakeAppProject",
-    description:
-        "Design of the Fake App Project page for both the desktop and mobile views",
-    icons: [
-        {
-            rel: "icon",
-            url: "favicon.ico",
-        },
-        {
-            rel: "icon",
-            type: "image/png",
-            sizes: "32x32",
-            url: "/favicon/favicon-32x32.png",
-        },
-        {
-            rel: "icon",
-            type: "image/png",
-            sizes: "16x16",
-            url: "/favicon/favicon-16x16.png",
-        },
-        {
-            rel: "apple-touch-icon",
-            sizes: "180x180",
-            url: "/favicon/apple-touch-icon.png",
-        },
-    ],
-};
+
 
 export default function RootLayout({ children }) {
     return (
